0e380293403ce577902fa7de20a335bf0e179f44
flveboy <flveboy@163.com>
2025-06-07 11:06:31 +0800

    Update index.ts
    update discuss info

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -151,10 +151,10 @@ const ExtendedTheme: Theme = {
     
     // Obtain configuration from: https://giscus.app/
     giscusTalk({
-      repo: 'Jackiexiao/nolebase-template',
-      repoId: 'R_kgDOL5WHsg',
+      repo: 'flveboy/nolebase-template',
+      repoId: 'R_kgDOOzHAkA',
       category: 'Announcements', // default: `General`
-      categoryId: 'DIC_kwDOL5WHss4CfTYs',
+      categoryId: 'DIC_kwDOOzHAkM4CrJQk',
       mapping: 'url', // default: `pathname`
       inputPosition: 'top', // default: `top`
       lang: 'zh-CN', // default: `zh-CN`
