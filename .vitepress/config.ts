fe21b52fb47b3c6723ba8b0a3a4c8c3a10645834
flveboy <flveboy@163.com>
2025-06-08 12:32:12 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -10,7 +10,7 @@ import { UnlazyImages } from '@nolebase/markdown-it-unlazy-img'
 
 import { discordLink, githubRepoLink, siteDescription, siteName, targetDomain } from '../metadata'
 import { creatorNames, creatorUsernames } from './creators'
-import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
+import { sidebar } from './docsMetadata.json'
 
 export default defineConfig({
   vue: {
@@ -210,13 +210,9 @@ export default defineConfig({
     nav: [
       { text: '主页', link: '/' },
       { text: '笔记', link: '/笔记/' },
-      { text: '万象视界', link: '/万象视界/'},
       { text: '最近更新', link: '/toc' },
     ],
-    sidebar: calculateSidebar([
-      { folderName: '/笔记', separate: true },
-      { folderName: '/万象视界', separate: true },
-    ], 'zh-CN'),
+    sidebar,
   },
   markdown: {
     theme: {
