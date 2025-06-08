934cd29eaf4e9dd37d79c78e27bbb43fbd1aebfd
爱永不凋零 <8510201+no225@user.noreply.gitee.com>
2025-06-08 10:43:57 +0800

    update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,18 +1,16 @@
-import { presetMarkdownIt } from '@nolebase/integrations/vitepress/markdown-it'
-import { transformHeadMeta } from '@nolebase/vitepress-plugin-meta'
-import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
-// import { buildEndGenerateOpenGraphImages } from '@nolebase/vitepress-plugin-og-image/vitepress'
+import process from 'node:process'
+import { defineConfig } from 'vitepress'
 import MarkdownItFootnote from 'markdown-it-footnote'
 import MarkdownItMathjax3 from 'markdown-it-mathjax3'
-import { defineConfig } from 'vitepress'
+
+import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links'
+import { InlineLinkPreviewElementTransform } from '@nolebase/vitepress-plugin-inline-link-preview/markdown-it'
+import { buildEndGenerateOpenGraphImages } from '@nolebase/vitepress-plugin-og-image/vitepress'
+import { UnlazyImages } from '@nolebase/markdown-it-unlazy-img'
 
 import { discordLink, githubRepoLink, siteDescription, siteName, targetDomain } from '../metadata'
 import { creatorNames, creatorUsernames } from './creators'
-
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
-import head from './head'
-
-const nolebase = presetMarkdownIt()
 
 export default defineConfig({
   vue: {
@@ -27,11 +25,122 @@ export default defineConfig({
       },
     },
   },
+  lang: 'zh-CN',
   title: siteName,
   description: siteDescription,
   ignoreDeadLinks: true,
-  head,
+  head: [
+    ['meta', {
+      name: 'theme-color',
+      content: '#ffffff',
+    }],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+    ],
+    ['link', {
+      rel: 'icon',
+      href: '/logo.svg',
+      type: 'image/svg+xml',
+    }],
+    [
+      'link',
+      {
+        rel: 'alternate icon',
+        href: '/favicon.ico',
+        type: 'image/png',
+        sizes: '16x16',
+      },
+    ],
+    ['meta', {
+      name: 'author',
+      content: creatorNames.join(', '),
+    }],
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          ['markdown', 'knowledge-base', '知识库', 'vitepress', 'obsidian', 'notebook', 'notes', ...creatorUsernames].join(', '),
+      },
+    ],
+
+    ['meta', {
+      property: 'og:title',
+      content: siteName,
+    }],
+    [
+      'meta',
+      {
+        property: 'og:image',
+        content: `${targetDomain}/og.png`,
+      },
+    ],
+    ['meta', {
+      property: 'og:description',
+      content: siteDescription,
+    }],
+    ['meta', {
+      property: 'og:site_name',
+      content: siteName,
+    }],
+
+    ['meta', {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    }],
+    ['meta', {
+      name: 'twitter:creator',
+      content: creatorUsernames.join(', '),
+    }],
+    [
+      'meta',
+      {
+        name: 'twitter:image',
+        content: `${targetDomain}/og.png`,
+      },
+    ],
+
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#927baf',
+      },
+    ],
+    ['link', {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    }],
+    ['meta', {
+      name: 'msapplication-TileColor',
+      content: '#603cba',
+    }],
+    // Proxying Plausible through Netlify | Plausible docs
+    // https://plausible.io/docs/proxy/guides/netlify
+    ['script', { 'defer': 'true', 'data-domain': 'nolebase.ayaka.io', 'data-api': '/api/v1/page-external-data/submit', 'src': '/assets/page-external-data/js/script.js' }],
+  ],
   themeConfig: {
+    outline: { label: '页面大纲', level: 'deep' },
+    darkModeSwitchLabel: '切换主题',
+    editLink: {
+      pattern: `${githubRepoLink}/tree/main/:path`,
+      text: '编辑本页面',
+    },
+    socialLinks: [
+      { icon: 'github', link: githubRepoLink },
+      { icon: 'discord', link: discordLink },
+    ],
+    footer: {
+      message: '用 <span style="color: #e25555;">&#9829;</span> 撰写',
+      copyright:
+        '<a class="footer-cc-link" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> © 2022-PRESENT Nólëbase 的创作者们',
+    },
     search: {
       provider: 'local',
       options: {
@@ -74,7 +183,7 @@ export default defineConfig({
 
           contentPart = content ||= src
 
-          const headingMatch = content.match(/^# .*/m)
+          const headingMatch = content.match(/^#{1} .*/m)
           const hasHeading = !!(headingMatch && headingMatch[0] && headingMatch.index !== undefined)
 
           if (hasHeading) {
@@ -115,29 +224,26 @@ export default defineConfig({
       dark: 'one-dark-pro',
     },
     math: true,
-    preConfig: async (md) => {
-      await nolebase.install(md)
-    },
     config: (md) => {
       md.use(MarkdownItFootnote)
       md.use(MarkdownItMathjax3)
+      md.use(BiDirectionalLinks({
+        dir: process.cwd(),
+      }))
+      md.use(UnlazyImages(), {
+        imgElementTag: 'NolebaseUnlazyImg',
+      })
+      md.use(InlineLinkPreviewElementTransform, {
+        tag: 'VPNolebaseInlineLinkPreview',
+      })
     },
   },
-  async transformHead(context) {
-    let head = [...context.head]
-
-    const returnedHead = await transformHeadMeta()(head, context)
-    if (typeof returnedHead !== 'undefined')
-      head = returnedHead
-
-    return head
+  async buildEnd(siteConfig) {
+    await buildEndGenerateOpenGraphImages({
+      baseUrl: targetDomain,
+      category: {
+        byLevel: 2,
+      },
+    })(siteConfig)
   },
-  // async buildEnd(siteConfig) {
-  //   await buildEndGenerateOpenGraphImages({
-  //     baseUrl: targetDomain,
-  //     category: {
-  //       byLevel: 2,
-  //     },
-  //   })(siteConfig)
-  // },
 })
